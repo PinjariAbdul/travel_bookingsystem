1321b9b28922d35873c3793712b15142aab275b2
abdulreigns.12345@gmail.com <abdulreigns.12345@gmail.com>
2025-08-29 11:40:30 +0530

    fix allowed hosts for render

--- travelbooking/settings.py
+++ travelbooking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9y9zsx61a(f&%yd^t+o*n&@k4k-s1kv&n7)=e$olc*296#s*-e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["travel-bookingsystem.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
